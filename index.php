2e9a9a4c91d7f8894be295d9c32df7e6a77eb6a5
Egor <left38331@mail.ru>
2017-08-27 16:20:35 +0300

    задание module2-task1

--- index.php
+++ index.php
@@ -112,27 +112,75 @@ $lot_time_remaining = time_add_zero($now, $tomorrow);
         <div class="lots__header">
             <h2>Открытые лоты</h2>
             <select class="lots__select">
-                <option>Все категории</option>
-                <option>Доски и лыжи</option>
-                <option>Крепления</option>
-                <option>Ботинки</option>
-                <option>Одежда</option>
-                <option>Инструменты</option>
-                <option>Разное</option>
+							
+							<?php 
+							// данные для select выбора обор-ия, заданного через массив
+							$categories = ['Доски и лыжи','Крепления','Ботинки','Одежда','Инструменты','Разное'];
+							$index= 0;
+							$number = count($categories);
+							while ($index < $number) {
+								$cat = $categories[$index];
+								print ('<option>' . $cat . '</option>');
+								$index = $index + 1;
+							} 
+							?>
+
             </select>
         </div>
         <ul class="lots__list">
+           
+					<?php 
+					$equipment = [
+						0 => [
+							'name' => '2014 Rossignol District Snowboard',
+							'category' => 'Доски и лыжи',
+							'price' => '159999',
+							'address' => 'img/lot-1.jpg'
+						],
+						1 => [
+							'name' => 'DC Ply Mens 2016/2017 Snowboard',
+							'category' => 'Доски и лыжи',
+							'price' => '10999',
+							'address' => 'img/lot-2.jpg'
+						],
+						2 => [
+							'name' => 'Крепления Union Contact Pro 2015 года размер L/XL',
+							'category' => 'Крепления',
+							'price' => '8000',
+							'address' => 'img/lot-3.jpg'
+						],
+						3 => [
+							'name' => 'Ботинки для сноуборда DC Mutiny Charocal',
+							'category' => 'Ботинки',
+							'price' => '10999',
+							'address' => 'img/lot-4.jpg'
+						],
+						4 => [
+							'name' => 'Куртка для сноуборда DC Mutiny Charocal',
+							'category' => 'Одежда',
+							'price' => '7500',
+							'address' => 'img/lot-5.jpg'
+						],
+						5 => [
+							'name' => 'Маска Oakley Canopy	',
+							'category' => 'Разное',
+							'price' => '5400',
+							'address' => 'img/lot-6.jpg'
+						]
+					];
+					?>
+						<?php foreach ($equipment as $key => $val): ?>
             <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src="img/lot-1.jpg" width="350" height="260" alt="Сноуборд">
+									<img src="<?=$val['address']; ?>" width="350" height="260" alt="<?=$val['category']; ?>">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category">Доски и лыжи</span>
+									<span class="lot__category"><?=$val['name']; ?></span>
                     <h3 class="lot__title"><a class="text-link" href="lot.html">2014 Rossignol District Snowboard</a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost">10 999<b class="rub">р</b></span>
+													<span class="lot__cost"><?=$val['price']; ?><b class="rub">р</b></span>
                         </div>
                         <div class="lot__timer timer">
                             <?=$lot_time_remaining;?>
@@ -140,6 +188,7 @@ $lot_time_remaining = time_add_zero($now, $tomorrow);
                     </div>
                 </div>
             </li>
+						<?php endforeach; ?>
         </ul>
     </section>
 </main>
