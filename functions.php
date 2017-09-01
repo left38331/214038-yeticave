7d856d7391b004ed959eb84eeb7dcc470aa29f93
Egor <left38331@mail.ru>
2017-09-01 08:52:26 +0300

    module3-task1

---
+++ functions.php
@@ -0,0 +1,15 @@
+<?php 
+
+function renderTemplate($path, $array) {
+	if (file_exists($path)) {
+		ob_start('ob_gzhandler');
+		extract($array, EXTR_SKIP);
+		require_once($path);
+		$html = ob_get_clean();
+		return $html;
+	} else {
+		return "";
+	}
+}
+
+?>
